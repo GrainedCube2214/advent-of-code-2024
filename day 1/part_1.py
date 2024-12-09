9b9e00fb1bd0c8041a6c735bf2aa25f52c234632
Ananth Bharathwaj T A <80443280+GrainedCube2214@users.noreply.github.com>
2024-12-09 15:00:44 +0530

    Day 1 done

---
+++ day 1/part_1.py
@@ -0,0 +1,15 @@
+file = open('D:/(arguably) projects/advent-of-code/day 1/input.txt', 'r')
+lines = file.readlines()
+file.close()
+lines = [line.split() for line in lines]
+l1 = []
+l2 = []
+for line in lines:
+    l1.append(int(line[0]))
+    l2.append(int(line[1]))
+l1.sort()
+l2.sort()
+sum = 0
+for i in range(len(l1)):
+    sum+= abs(l1[i]-l2[i])
+print(sum)
---
+++ day 1/part_2.py
@@ -0,0 +1,13 @@
+file = open('D:/(arguably) projects/advent-of-code/day 1/input.txt', 'r')
+lines = file.readlines()
+file.close()
+lines = [line.split() for line in lines]
+l1 = []
+l2 = []
+for line in lines:
+    l1.append(int(line[0]))
+    l2.append(int(line[1]))
+sum = 0
+for i in l1:
+    sum+=i*l2.count(i)
+print(sum)
